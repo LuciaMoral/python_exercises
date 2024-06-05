62c86076349e255ae9ea5d6196a0a3d91a979f53
Lucia Moral <lmoralbaena@gmail.com>
2024-06-05 19:14:09 +0200

    completed fibonacci exercise

---
+++ Week_3/fibonacci_seq.py
@@ -0,0 +1,15 @@
+class Fibonacci:
+    def __init__(self, number):
+        self.number = number
+
+    def calculate_sequence(self):
+        first = 0
+        second = 1
+        for i in range(self.number):
+            print(first)
+            temp = first
+            first = second
+            second = temp + second
+
+sequence = Fibonacci(30)
+sequence.calculate_sequence()
